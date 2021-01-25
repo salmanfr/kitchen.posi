6f5d860ab75edb1be958fe359c813d10a1ef7c14
SALMAN FAJAR RIZKI <31918709+salmanfr@users.noreply.github.com>
2021-01-25 22:09:18 +0700

    delete folder

--- thisExample/exit.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION['jenjang'] = '';
-$_SESSION['email'] = '';
-$_SESSION['kontak'] = '';
-$_SESSION['token'] = '';
-session_destroy();
-header('location:index.html');
-?>
