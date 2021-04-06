f0debd8e4102b1a652ba1ac2d25dc6b0593c159a
SALMAN FAJAR RIZKI <31918709+salmanfr@users.noreply.github.com>
2021-04-06 13:42:13 +0700

    Delete

--- app/app-assets/js/setckeditor.js
+++
@@ -1,9 +0,0 @@
-var initSample = (function() {
-    return function(domId, tinggi) {
-        CKEDITOR.replace(domId, {
-            extraPlugins: 'ckeditor_wiris,mathjax,pastebase64,imageku,imageresizerowandcolumn,justify',
-            mathJaxLib: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-AMS_HTML',
-            height: tinggi
-        });
-    };
-})();
